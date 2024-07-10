239c23acfca88ce350559ba4df58bd0599be7bd8
JuliaRajn <rajnjulia@gmail.com>
2024-07-10 15:52:23 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+DZ='12'
+hour='1.5'
+name='Python'
+all_hour = float(hour)/int(DZ)
+print('Курс:'+  name + ', всего задач:'+ DZ + ', затрачено часов:', hour + ', среднее время выполнения', all_hour)
